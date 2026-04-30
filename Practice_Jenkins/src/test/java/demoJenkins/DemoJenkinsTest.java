bbde04d5ca7244146c92b95a06e39e66b69a4eb4
Naresh <Naresh@Naresh>
2026-04-30 11:03:25 +0530

    This is commit for jenkins Practice

---
+++ Practice_Jenkins/src/test/java/demoJenkins/DemoJenkinsTest.java
@@ -0,0 +1,12 @@
+package demoJenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoJenkinsTest {
+
+	@Test
+public void test() {
+	Reporter.log(">>>>>>>>>>>Hi from jenkins*************<<<<<<<<<<<<<",true);
+}
+}
