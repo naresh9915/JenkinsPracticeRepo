29cdbd4577a1470069fceeb0a7a2a267976e7e87
Naresh Kumar <naresh9915@gmail.com>
2026-06-04 18:37:21 +0530

    5th commit

--- Practice_Jenkins/src/test/java/demoJenkins/DemoJenkinsTest.java
+++ Practice_Jenkins/src/test/java/demoJenkins/DemoJenkinsTest.java
@@ -12,6 +12,7 @@ public void test() {
 	Reporter.log(">>>>>>>>>>>Hi from jenkins2*************<<<<<<<<<<<<<",true);	
 	
 	
+	
 	Reporter.log(">>>>>>>>>>>Hi from jenkins3*************<<<<<<<<<<<<<",true);	
 }
 }
